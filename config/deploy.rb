55cf2b40ac95e0bf2f807961697488f8298de660
Jacko Zuidema <jackozuidema@gmail.com>
2014-03-15 00:08:03 +0100

    assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@bitbucket.org:zuidemaj/hamers.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
- set :deploy_to, '/var/www/wiegaaternaardebeiaard.nl/current'
+ set :deploy_to, '/var/www/wiegaaternaardebeiaard.nl'
  set :rails_env, "production"
 # Default value for :scm is :git
 # set :scm, :git
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Hamers::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -33,7 +33,7 @@ Hamers::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
