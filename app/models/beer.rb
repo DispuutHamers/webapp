22c9d9f1d8009fe58bb9bf2fa6b0e64f0858054a
Jacko Zuidema <jackozuidema@gmail.com>
2014-04-22 00:44:07 +0200

    gem. cijfer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,12 @@ class Beer < ActiveRecord::Base
    self.reviews.create!(user_id: user.id, rating: rating, description: description)
  end
  
+ def cijfer?
+   cijfer = 0
+   self.reviews.each do |r| 
+     cijfer = cijfer + r.rating
+   end
+   return (cijfer / self.reviews.count)
+ end 
+ 
 end
