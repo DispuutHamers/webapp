27027cbd9c18b59b6953a176f7f83ea49ca6ad1e
Jacko Zuidema <jackozuidema@gmail.com>
2014-04-22 04:31:55 +0200

    random biertje

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   include SessionsHelper
   def home
+    @beer = Beer.find(:all, :limit => 1, :order => "RAND()")
     @quote = current_user.quotes.build if signed_in?
     @feed_items = current_user.feed.paginate(page: params[:page]) if signed_in?
     @events = Event.find(:all, 
