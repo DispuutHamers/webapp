a763d1a3b19c97375f4b4f5ab0d01dcb7eaf72d8
Jacko Zuidema <jackozuidema@gmail.com>
2014-11-02 19:37:31 +0100

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in?, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in?, except: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user?, only: [:destroy, :usergroups]
 
