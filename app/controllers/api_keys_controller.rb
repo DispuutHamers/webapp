842d9120534d388cf3d10577986d668fecd18bb1
Jacko Zuidema <jackozuidema@gmail.com>
2015-01-04 15:05:45 +0100

    security fix

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,5 +1,6 @@
 class ApiKeysController < ApplicationController
 	before_action :logged_in?
+	before_action :correct_user?, only: [:show]
 
   def create
 		current_user.generate_api_key(api_key_params[:name])
@@ -21,4 +22,8 @@ class ApiKeysController < ApplicationController
 		def api_key_params
 			params.require(:api_key).permit(:name)
 		end 
+
+		def correct_user? 
+			redirect_to root_url, notice: "Niet genoeg access bitch" if current_user == ApiKey.find(params[:id]).user || admin_user?
+		end
 end
