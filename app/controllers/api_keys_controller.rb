21869bd5cebadf4faafc2086afb4520fa37d1783
Dex Bleeker <dex@bleeker.nl>
2015-01-04 17:00:41 +0100

    Nu de goede unit

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -23,7 +23,7 @@ class ApiKeysController < ApplicationController
 			params.require(:api_key).permit(:name)
 		end 
 
-		def current_user.admin?
-			redirect_to root_url, notice: "Niet genoeg access bitch" if current_user == ApiKey.find(params[:id]).user || admin_user?
+		def current_user?
+			redirect_to root_url, notice: "Niet genoeg access bitch" if current_user.admin? == ApiKey.find(params[:id]).user || admin_user?
 		end
 end
