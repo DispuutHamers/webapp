82481a012b36a4f1470a766bfd990ff566bd3918
Jacko Zuidema <jackozuidema@gmail.com>
2015-05-04 12:19:25 +0200

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
 			@quote = current_user.quotes.build 
 			@feed_items = current_user.feed.paginate(page: params[:page], :per_page => 5) 
 			@events = Event.order("date").where(['date >= ?', Date.today]).limit(10)
-			@news = News.last(2)
+			@news = News.last(2).reverse
 		end
   end
 end
