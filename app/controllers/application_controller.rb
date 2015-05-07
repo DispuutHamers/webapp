e8f843c7789ebed0f4a43f6a75324f99e97d2e63
Jacko Zuidema <jackozuidema@gmail.com>
2015-05-07 21:06:47 +0200

    caching for back button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	before_filter :set_cache_buster
 	include SessionsHelper
 	include ParamsHelper
 
-    def logged_in?
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+	def set_cache_buster
+		response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+		response.headers["Pragma"] = "no-cache"
+		response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+	end
 
-    def admin_user?
-			logged_in?
-      redirect_to root_url, notice: "Niet genoeg access bitch" unless (current_user.admin? || current_user.schrijf_feut? || current_user.dev?)
-    end
+
+	def logged_in?
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+	def admin_user?
+		logged_in?
+    redirect_to root_url, notice: "Niet genoeg access bitch" unless (current_user.admin? || current_user.schrijf_feut? || current_user.dev?)
+  end
 end
