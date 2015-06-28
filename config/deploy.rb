fafe8dfee5c922d4913be25ac86a14c0236572bc
Jacko Zuidema <jackozuidema@gmail.com>
2015-06-28 17:07:57 +0200

    whenever cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
 # config valid only for Capistrano 3.1
+require "whenever/capistrano"
+set :whenever_command, "bundle exec whenever"
 
 set :application, 'Hamers'
 set :repo_url, 'git@bitbucket.org:jackozi/hamers.git'
