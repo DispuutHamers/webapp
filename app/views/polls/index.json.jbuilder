31d8c54db62d1265d2ec58375dd8c66b8f93048e
Dex Bleeker <dex@bleeker.nl>
2015-08-04 01:54:39 +0200

    Polls deleted

--- app/views/polls/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@polls) do |poll|
-  json.extract! poll, :id, :beschrijving
-  json.url poll_url(poll, format: :json)
-end
--- db/migrate/20140324165427_create_polls.rb
+++
@@ -1,9 +0,0 @@
-class CreatePolls < ActiveRecord::Migration
-  def change
-    create_table :polls do |t|
-      t.string :beschrijving
-
-      t.timestamps
-    end
-  end
-end
