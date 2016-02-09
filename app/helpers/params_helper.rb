c48e509fa70c6d4585190d530573c359df949489
Dex Bleeker <dex@bleeker.nl>
2016-02-09 15:23:49 +0100

    Code styling: Helpers

--- app/helpers/params_helper.rb
+++ app/helpers/params_helper.rb
@@ -1,38 +1,38 @@
 module ParamsHelper
 
-	def review_params
-		params.require(:review).permit(:user_id, :beer_id, :description, :rating, :proefdatum)
-	end
+  def review_params
+    params.require(:review).permit(:user_id, :beer_id, :description, :rating, :proefdatum)
+  end
 
-	def micropost_params
-		params.require(:quote).permit(:user_id, :text)
-	end
+  def micropost_params
+    params.require(:quote).permit(:user_id, :text)
+  end
 
-	def event_params
-		params.require(:event).permit(:end_time, :deadline, :user_id, :beschrijving, :title, :date, :location)
-	end
+  def event_params
+    params.require(:event).permit(:end_time, :deadline, :user_id, :beschrijving, :title, :date, :location)
+  end
 
-	def motion_params
-		params.require(:motion).permit(:motion_type, :subject, :content)
-	end
+  def motion_params
+    params.require(:motion).permit(:motion_type, :subject, :content)
+  end
 
-	def beer_params
-		params.require(:beer).permit(:name, :soort, :picture, :percentage, :country, :brewer, :URL)
-	end
+  def beer_params
+    params.require(:beer).permit(:name, :soort, :picture, :percentage, :country, :brewer, :URL)
+  end
 
-	def news_params
-		params.require(:news).permit(:cat, :body, :title, :image, :date)
-	end
+  def news_params
+    params.require(:news).permit(:cat, :body, :title, :image, :date)
+  end
 
-	def device_params
-		params.require(:device).permit(:device_key)
-	end
+  def device_params
+    params.require(:device).permit(:device_key)
+  end
 
-	def arm_params
-		params.requre(:arm).permit(:lat, :lon)
-	end
- 
-        def nickname_params
-	      params.require(:nickname).permit(:user_id, :nickname, :description)
-        end
+  def arm_params
+    params.requre(:arm).permit(:lat, :lon)
+  end
+
+  def nickname_params
+    params.require(:nickname).permit(:user_id, :nickname, :description)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = {size: 50})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
