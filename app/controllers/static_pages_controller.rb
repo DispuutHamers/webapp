d0e6e0e8dac829e833b5c0cbf66a34484f9c98b7
rob <rob.stortelder@me.com>
2016-04-04 18:07:14 +0200

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,7 @@ class StaticPagesController < ApplicationController
   end
 
   def console
-    redirect_to root_path unless current_user&.admin?
+    redirect_to root_path unless current_user.admin?
   end
 
   def statistics
