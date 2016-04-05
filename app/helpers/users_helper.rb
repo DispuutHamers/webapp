6c04a2ffa51c1b2428a9e635b7e9403fbae97997
Dex Bleeker <dex@bleeker.nl>
2016-04-05 14:44:51 +0200

    Cleaner users page-layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
 
-  def gravatar_for(user, options = {size: 50})
+  def gravatar_for(user, options = {size: 50, class: 'gravatar img img-repsonsive'})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    html_class = options[:class]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name, class: html_class)
   end
 
 end
