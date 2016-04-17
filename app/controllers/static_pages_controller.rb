8ea5f9e812fe14561f8a0713dde0aac767035335
Jacko Zuidema <jackozuidema@gmail.com>
2016-04-17 13:14:20 +0000

    static_pages_controller.rb edited online with Bitbucket

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @beer = Beer.order('RAND()').first
-      @rQuote = Quote.order('RAND()').first
+      @rQuote = Quote.unscoped.order('RAND()').first
       @quote = current_user.quotes.build
       @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 8)
       @events = Event.order('date').where(['date >= ?', Date.today]).limit(10)
