1e284df51530f4a484406d0d47276da6e2616df4
Jacko Zuidema <jackozuidema@gmail.com>
2016-06-06 14:12:06 +0200

    logging

--- app/controllers/api2/api_controller.rb
+++ app/controllers/api2/api_controller.rb
@@ -1,5 +1,6 @@
 class Api2::ApiController < ApplicationController
 	before_action :restrict_access
+	before_action :log_url
 	include ParamsHelper
 	#resource_description do
 	#	  api_versions "2"
@@ -20,5 +21,9 @@ class Api2::ApiController < ApplicationController
 			@key = ApiKey.where(key: token).first
 			@key && @key.user && @key.user.lid?
 		end
+	end
+
+	def log_url
+		ApiLog.new(key: @key.key, user_id: @key.user.id, ip_addr: request.remote_ip, resource_call: (request.method + ": " + request.original_fullpath + " " + params.to_s)).save
 	end
 end
