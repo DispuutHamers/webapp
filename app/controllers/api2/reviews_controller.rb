c7ef2c786722271ffcddec2bfcd455afc7829f3e
Jacko Zuidema <jackozuidema@gmail.com>
2016-06-09 15:58:11 +0200

    fix

--- app/controllers/api2/reviews_controller.rb
+++ app/controllers/api2/reviews_controller.rb
@@ -37,9 +37,10 @@ class Api2::ReviewsController < Api2::ApiController
 	param :proefdatum, DateTime
 	def create
 	  @review = Review.new(review_params)
-	  @beer = Beer.find(params[:review][:beer_id]) # Nog nakijken voor injection
+	  @beer = Beer.find(params[:review][:beer_id]) 
 	  reviews = @key.user.reviews.where(beer_id: @beer.id)
 	  render json: @beer, status: :unprocessable_entity and return if reviews.any?
+		@review.user_id = @key.user.id
 	  @review.proefdatum = Date.today unless @review.proefdatum
 	  if @review.save
             update_app("{ data: { review: { beer_id: \"#{@review.beer_id}\", user_id: \"#{@review.user_id}\", description: \"#{@review.description}\", rating: \"#{@review.rating}\", proefdatum: #{@review.proefdatum.to_json}, created_at: #{@review.created_at.to_json}} } }")
