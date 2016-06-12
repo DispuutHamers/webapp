635af00c6596cac322c804d6289f320067efc410
Jacko Zuidema <jackozuidema@gmail.com>
2016-06-12 11:19:26 +0200

    fixes

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,6 +1,7 @@
 class Signup < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+	validates :user_id, uniqueness: {scope: :event_id}
 
 	def as_json(options)
 		h = super({:only => [:id, :user_id, :event_id, :status, :created_at]}.merge(options))
