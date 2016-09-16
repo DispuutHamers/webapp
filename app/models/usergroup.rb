a0fde4ff84768df6d020bccfef467e7994a045a2
Jacko Zuidema <jackozuidema@gmail.com>
2016-09-16 14:25:47 +0200

    option to remove empty usergroups

--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -1,4 +1,8 @@
 class Usergroup < ActiveRecord::Base
 	acts_as_paranoid
   has_many :groups, foreign_key: 'group_id', dependent: :destroy
+
+	def empty?
+		Group.where(group_id: id).count == 0
+	end
 end
