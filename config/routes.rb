97cf24b91085f33bea73aefc49d4b491338dd2a2
Jacko Zuidema <jackozuidema@gmail.com>
2016-09-16 11:01:34 +0200

    backups

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,7 @@ Hamers::Application.routes.draw do
 			resources :reviews, module: 'api2', only: [:index, :show, :update, :create]
 			resources :events, module: 'api2', only: [:index, :show, :update, :create]
 			resources :motions, module: 'api2', only: [:index, :show, :update, :create]
+			get 'full_backup' => 'api2/backups#index'
 			get 'whoami' => "api2/users#whoami"
 			post 'register' => "api2/api#register"
 		end
