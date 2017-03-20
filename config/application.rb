91803a32d10db913cb9389d59adeaa5cec27cc49
Jacko Zuidema <jackozuidema@gmail.com>
2017-03-20 16:43:53 +0100

    timezone setting

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Hamers
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.web_console.development_only = false
+    config.time_zone = "Amsterdam"
     config.i18n.default_locale = :'nl'
   end
 end
