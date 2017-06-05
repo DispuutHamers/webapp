44c7c40e7209cbf22927f67b8f8b9e9db6acc449
Jacko Zuidema <jackozuidema@gmail.com>
2017-06-06 00:56:22 +0200

    mehb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,9 +6,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-  end
-
 end
