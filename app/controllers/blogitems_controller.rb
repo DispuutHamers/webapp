4b083397d58d805c53a5aebdae001ceda8ba8a98
Jacko Zuidema <jackozuidema@gmail.com>
2017-06-05 15:48:59 +0200

    Fix voor blog

--- app/controllers/blogitems_controller.rb
+++ app/controllers/blogitems_controller.rb
@@ -58,6 +58,6 @@ class BlogitemsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_item
-    @item = Blogitem.find(params[:id])
+    @item = Blogitem.unscoped.find(params[:id])
   end
 end
