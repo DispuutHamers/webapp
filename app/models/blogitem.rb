3b0e7a286b0f145b2111d2a632e5908fb84e0a11
Jacko Zuidema <jackozuidema@gmail.com>
2017-06-06 19:22:36 +0200

    fix

--- app/models/blogitem.rb
+++ app/models/blogitem.rb
@@ -1,6 +1,7 @@
 class Blogitem < ApplicationRecord
   has_paper_trail on: [:update]
-  serialize :body, :title
+  serialize :body
+  serialize :title
   has_many :blogphotos, dependent: :destroy
 
   def self.default_scope
