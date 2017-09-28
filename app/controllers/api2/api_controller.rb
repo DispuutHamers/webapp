e723c96bf7fd153f076dbf8007df44c6c34bd297
Jacko Zuidema <jackozuidema@gmail.com>
2017-09-28 14:32:20 +0200

    API Fixes

--- app/controllers/api2/api_controller.rb
+++ app/controllers/api2/api_controller.rb
@@ -29,9 +29,9 @@ class Api2::ApiController < ApplicationController
     user = @key.user
     device.assign_attributes(user_id: user.id, device_key: params[:device])
     if device.save
-      render :status => :created, :text => '{"status":"201","message":"Created"}'
+      render :status => :created, :plain => '{"status":"201","message":"Created"}'
     else
-      render :status => :bad_request, :text => '{"status":"400","error":"Bad request"}'
+      render :status => :bad_request, :plain => '{"status":"400","error":"Bad request"}'
     end
   end
 
