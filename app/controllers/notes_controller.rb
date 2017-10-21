0d79ce749e371750c6cc0d6e705fe3918a3b9d12
Jacko Zuidema <jackozuidema@gmail.com>
2017-10-21 14:16:52 +0200

    make notes for logged_in users

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 # entry point for notes
 class NotesController < ApplicationController
-  before_action :admin_user?
+  before_action :logged_in?
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   # GET /notes
