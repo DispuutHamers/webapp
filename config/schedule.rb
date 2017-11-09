e37fe5a9c3cdd238a7d466fbc8bc49f35d083183
Jacko Zuidema <jackozuidema@gmail.com>
2017-11-09 17:22:40 +0100

    omschrijving zondagborrel

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every :sunday, :at => '12pm' do # Use any day of the week or :weekend, :weekday
-  runner "Event.new(beschrijving: \"Zondagborrel\", attendance: true, title: \"Zondagborrel\", date: Time.now + 1.weeks + 9.hours, deadline: Time.now + 1.weeks + 8.hours, end_time: Time.now + 1.weeks + 14.hours, location: \"Beiaard Enschede\").save"
+  runner "Event.new(beschrijving: \"Tijdenlang heeft hier slechts het woord Zondagborrel gestaan, wat uiteraard de volledige dekking van dit wekelijks ritueel uitmuntent omschrijft, echter nieuwe tijden vragen om een nieuwe beschrijving. Daarom hetzelfde evenement maar nu met een langere omschrijving zodat idereen iets te lezen zal hebben terwijl `hun` dit grandiose event in spanning afwachten.\", attendance: true, title: \"Zondagborrel\", date: Time.now + 1.weeks + 9.hours, deadline: Time.now + 1.weeks + 8.hours, end_time: Time.now + 1.weeks + 14.hours, location: \"Beiaard Enschede\").save"
 end
 
 every :friday, :at => "7pm" do
