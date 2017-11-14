e518ca1b5723f338906fa2d79789edfe601ad857
Jacko Zuidema <jackozuidema@gmail.com>
2017-11-14 20:12:02 +0100

    land voor rop

--- config/initializers/beer_filter.rb
+++ config/initializers/beer_filter.rb
@@ -5,4 +5,19 @@ class BeerFilter
     columns = [:name, :brewer, :soort]
     scope.where_like(columns => phrases)
   end
+
+  filter :land do |scope, phrases|
+    columns = [:country]
+    scope.where_like(columns => phrases)
+  end
+
+  filter :brouwer do |scope, phrases|
+    columns = [:brewer]
+    scope.where_like(columns => phrases)
+  end
+
+  filter :naam do |scope, phrases|
+    columns = [:name]
+    scope.where_like(columns => phrases)
+  end
 end
