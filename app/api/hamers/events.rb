80b99efd4c89757defd4e3bc98e4626a979e84f0
Jacko Zuidema <jackozuidema@gmail.com>
2017-12-19 18:54:43 +0100

    more api methods for events

--- app/api/hamers/events.rb
+++ app/api/hamers/events.rb
@@ -1,16 +1,17 @@
 module Hamers
   class Events < Grape::API
     resources :events do
-      desc 'Get all events'
+      desc 'Selecteer alle events'
       oauth2
       get do
         Event.all
       end
 
-      desc 'Get a specific event'
+      desc 'Selecteer een specifiek event bij id'
       params do
         requires :id, type: Integer, desc: 'Event id'
       end
+
       route_param :id do
         oauth2
         get do
@@ -18,7 +19,7 @@ module Hamers
         end
       end
 
-      desc 'Create an event'
+      desc 'Create een event'
       params do
         requires :beschrijving, type: String, desc: 'Beschrijving van het event'
         requires :title, type: String, desc: 'Naam van het event'
@@ -27,6 +28,7 @@ module Hamers
         requires :endtime, type: DateTime, desc: 'Wanneer is het event afgelopen'
         optional :location, type: String, desc: 'Waar is het event'
       end
+
       oauth2
       post do
         location = params[:location] ||= "TBD"
@@ -39,6 +41,43 @@ module Hamers
           end_time: params[:endtime],
           location: location
         })
+      end
+
+      desc 'Update een event'
+      params do
+        requires :id, type: Integer, desc: 'Event id'
+          optional :title, type: String, desc: 'Nieuwe title voor het event'
+          optional :beschrijving, type: String, desc: 'Nieuwe beschrijving voor het event'
+          optional :date, type: DateTime, desc: 'Nieuwe starttijd voor het event, stoptijd en deadline worden niet aangepast.'
+          optional :deadline, type: DateTime, desc: 'Nieuwe inschrijf deadline voor het event'
+          optional :endtime, type: DateTime, desc: 'Nieuwe eindtijd voor het event, starttijd en deadline worden niet aangepast'
+          optional :location, type: String, desc: 'Nieuwe locatie voor het event'
+      end
+
+      oauth2
+      route_param :id do
+        put do
+          Event.find(params[:id]).update({
+            beschrijving: params[:beschrijving],
+            title: params[:title],
+            date: params[:date],
+            deadline: params[:deadline],
+            end_time: params[:endtime],
+            location: params[:location]
+          })
+        end
+      end
+
+      desc 'Delete een event'
+      params do
+        requires :id, type: Integer, desc: 'Event id'
+      end
+
+      oauth2
+      route_param :id do 
+        delete do
+          Event.find(params[:id]).destroy
+        end
       end
     end
   end
