ab8ed28a340e08e89ed049f13e86540d8310de4a
Jacko Zuidema <jackozuidema@gmail.com>
2017-12-29 16:29:17 +0100

    api layout settings

--- config/initializers/swagger_ui_engine.rb
+++ config/initializers/swagger_ui_engine.rb
@@ -2,4 +2,8 @@ SwaggerUiEngine.configure do |config|
   config.swagger_url = {
     v3: 'https://zondersikkel.nl/api/v3/swagger_doc'
   }
+
+  config.model_rendering = 'model'
+  config.validator_enabled = true
+
 end
