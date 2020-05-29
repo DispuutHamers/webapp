636af0265fb6ab4341eb4a80927ca5057cf742d1
Jacko Zuidema <jackozuidema@gmail.com>
2020-05-29 18:39:59 +0200

    make reservation on sunday

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 # Sends emails to our users
 class UserMailer < ApplicationMailer
+  layout 'mailer', only: %i[mail_event_reminder]
+  layout 'reservation', only: %i[mail_reservation]
 
   def mail_event_reminder(user, event)
     @user = user
     @event = event
     mail(to: @user.email, subject: "Doe eens inschrijven...")
+  end
+
+  def mail_reservation(event)
+    mail(to: 'han@devluchte.nl', subject: "Hamers: #{event.signups.where(status: true).count} personen")
   end
 
 end
