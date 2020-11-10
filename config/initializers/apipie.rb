56508e493f7ac5fb0ea1d85f7f672742f4ecc461
Dex Bleeker <dex@bleeker.nl>
2020-11-10 16:07:12 +0100

    Remove apipie config

--- config/initializers/apipie.rb
+++
@@ -1,7 +0,0 @@
-Apipie.configure do |config|
-  config.app_name                = "Hamers"
-  config.api_base_url            = "/api/v2"
-  config.doc_base_url            = "/apidocs"
-  # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api2/*.rb"
-end
