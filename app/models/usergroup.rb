fe96cab323a01af1a7ec977b4c167f3cc8f03adb
Dex Bleeker <dex@bleeker.nl>
2020-11-10 23:08:36 +0100

    Layout fixes

--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -1,6 +1,7 @@
 # Usergroup class
 class Usergroup < ActiveRecord::Base
   acts_as_paranoid
+  belongs_to :user
   has_many :groups, foreign_key: 'group_id', dependent: :destroy
   has_many :users, through: :groups
 
