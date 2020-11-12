3e5b09ccae528b5145bf7d148c66faace3a59724
Dex Bleeker <dex@bleeker.nl>
2020-11-12 10:55:39 +0100

    user.meetings --> user.meetings_attended

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ remember_token unconfirmed_email failed_attempts unlock_token locked_at weight u
   has_many :nicknames
   has_many :blogitems
   has_many :attendees
-  has_many :meetings, through: :attendees
+  has_many :meetings_attended, through: :attendees, source: :meeting
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
