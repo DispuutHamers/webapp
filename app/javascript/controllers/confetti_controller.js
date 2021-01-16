69291fbd43fe887f7dbd9f4154f8ccaf98fa8cfa
Jacko Zuidema <jackozuidema@gmail.com>
2021-01-16 11:39:20 +0100

    controller cleanup

--- app/javascript/controllers/confetti_controller.js
+++ app/javascript/controllers/confetti_controller.js
@@ -5,22 +5,21 @@ export default class extends Controller {
   static targets = [ "canvas" ]
 
   start() {
-    if (typeof this.confetti === 'undefined') {
-      var confettiSettings = {
-        target: this.canvasTarget,
-        size: 1.2,
-        clock: 30,
-        rotate: true
-      };
-      this.confetti = new ConfettiGenerator(confettiSettings);
-    }
-    console.log('Starting render');
+    this.confetti = this.confetti();
     this.confetti.render();
-    console.log('Stopping render');
   }
 
   stop() {
     this.confetti.clear();
-    this.confetti = undefined;
+    delete this.confetti;
+  }
+
+  confetti() {
+    return new ConfettiGenerator({
+      target: this.canvasTarget,
+      size: 1.2,
+      clock: 30,
+      rotate: true
+    });
   }
 }
