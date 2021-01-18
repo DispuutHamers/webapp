72feaa7c2c503f92a07a82ce451892364c0d6fdb
Dex Bleeker <dex@bleeker.nl>
2021-01-18 16:05:33 +0100

    Add secure headers gem and CSP headers

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,13 @@
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true,
+    httponly: true
+  }
+  config.csp = {
+    default_src: %w('self' www.google.com),
+    base_uri: %w('self'),
+    img_src: %w(*),
+    script_src: %w('self' www.google.com www.gstatic.com cdnjs.cloudflare.com),
+    style_src: %w('self' 'unsafe-inline' www.gstatic.com cdnjs.cloudflare.com),
+  }
+end
