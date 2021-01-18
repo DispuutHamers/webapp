95a15996fee77e194c7073ee74084363ec008b5e
Dex Bleeker <dex@bleeker.nl>
2021-01-18 17:19:41 +0100

    Fix display of editor icons (CSP too strict)

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,7 +6,7 @@ SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self' www.google.com),
     base_uri: %w('self'),
-    img_src: %w(*),
+    img_src: %w(* data:),
     script_src: %w('self' www.google.com www.gstatic.com cdnjs.cloudflare.com),
     style_src: %w('self' 'unsafe-inline' www.gstatic.com cdnjs.cloudflare.com),
   }
