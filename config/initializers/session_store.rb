801ab0ef31b44ea6a44be91594ae9b4352774b04
dexbleeker <dex@bleeker.nl>
2021-08-24 13:41:57 +0200

    Do not enforce secure headers in development environment (#180)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,10 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, {
-  key: '_hamers_session',
-  same_site: :none,
-  secure: :true
-}
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, {
+    key: '_hamers_session',
+    same_site: :none,
+    secure: :true
+  }
+else
+  Rails.application.config.session_store :cookie_store
+end
