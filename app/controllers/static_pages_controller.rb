a44b04ca3dc9c709ee74580ca46bd1dc2834152b
Dex Bleeker <dex@bleeker.nl>
2021-08-28 17:56:08 +0200

    WIP

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,22 @@
 # Static pages controller
 class StaticPagesController < ApplicationController
-  before_action :ilid?, only: %i[home trail revert]
+  before_action :ilid?, only: %i[trail revert]
 
   def home
-    @random_beer = Beer.random.take
-    @random_quote = Quote.random.take
-    @quote = current_user.quotes.build
-    @blog = Blogitem.last(5).reverse
-    @quotes = Quote.with_user.ordered.paginate(page: params[:page], :per_page => 12)
-    @events = Event.order('date').where(['date >= ?', Date.today]).limit(5)
-    @news = News.last(5).reverse
-    @trail = PaperTrail::Version.includes(:item).last(5).reverse
-  end
+    if current_user&.active?
+      @random_beer = Beer.random.take
+      @random_quote = Quote.random.take
+      @quote = current_user.quotes.build
+      @blog = Blogitem.last(5).reverse
+      @quotes = Quote.with_user.ordered.paginate(page: params[:page], :per_page => 12)
+      @events = Event.order('date').where(['date >= ?', Date.today]).limit(5)
+      @news = News.last(5).reverse
+      @trail = PaperTrail::Version.includes(:item).last(5).reverse
 
-  def frontpage
-    layout 'application'
+      render layout: 'static_pages/home'
+    else
+      render 'frontpage'
+    end
   end
 
   def privacy
