09ac57c47461404c21d676148cb3b08ea39705ae
Dex Bleeker <dex@bleeker.nl>
2021-08-28 13:19:12 +0200

    Rewrite and cleanup schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,23 +1,21 @@
 set :chronic_options, hours24: true
 
 every :wednesday, at: "1200" do
-  # Use any day of the week or :weekend, :weekday
+  date = "Time.now + 1.weeks + 9.hours" # Next wednesday, 21:00h
+  end_time = "Time.now + 1.weeks + 14.hours" # Next thursday, 02:00h
+  deadline = "Time.now + 1.weeks + 6.hours" # Next wednesday, 18:00h
   runner "Event.new(description: \"Woensdag, wellicht de mooiste dag van de week. Voor sommigen de dag van God, maar voor ons voornamelijk de dag van bier! Na enkele weken onder de tyrannie van slecht gespelde evenementen is ook deze automatische uitnodiging danig ververst, dat het er fatsoenlijk uitziet. Wij moeten er ook fatsoenlijk uitzien, dus overhemd aan en naar de Vluchte!\",
                     attendance: true, title: \"Dispuutsborrel\",
-                    date: Time.now + 1.weeks + 9.hours,
-                    deadline: Time.now + 1.weeks + 6.hours,
-                    end_time: Time.now + 1.weeks + 14.hours,
-                    location: \"De Vluchte Enschede\"
-          ).save"
+                    date: #{date},
+                    end_time: #{end_time},
+                    deadline: #{deadline},
+                    location: \"De Vluchte\"
+                   ).save"
 end
 
-every :sunday, at: "1900" do
-  runner 'UtilHelper.remind_zondag'
+every :tuesday, at: "1900" do
+  runner 'UtilHelper.remind_drink'
 end
-
-# every :sunday, :at => "7pm" do
-#   runner 'UtilHelper.make_reservation'
-# end
 
 every :day, at: '0605' do
   runner 'User.leden.each{ |u| UsersHelper.update_weight_for(u) }'
