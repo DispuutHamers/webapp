40b4852bc62a68af12c8b202e3da388106e446fd
Dex Bleeker <dex@bleeker.nl>
2021-09-09 20:46:54 +0200

    Set proefdatum to today manually

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,8 +9,13 @@ class Review < ActiveRecord::Base
   validates :user, uniqueness: {scope: [:user, :beer]}, presence: true
   validates :beer, presence: true
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10, only_integer: true }
+  after_initialize :set_proefdatum
 
   has_rich_text :actiontext_description
 
   scope :with_user, -> { includes(:user) }
+
+  def set_proefdatum
+    self.proefdatum ||= Date.today
+  end
 end
