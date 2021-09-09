87fc6ffc1c60adb27fb8fd7857ce00c69dd8179a
Dex Bleeker <dex@bleeker.nl>
2021-09-09 12:46:37 +0200

    Test review model

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,12 +2,48 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
   setup do
-    # New beer to rule out existing reviews interfering with our test.
-    @beer = Beer.create(name: "Testbier")
     @user = users(:one)
+    # New beer to rule out existing reviews interfering with our test.
+    @beer = Beer.new(name: "Testbier")
+    @review = Review.new(beer: @beer, user: @user, rating: 8, actiontext_description: "Goed bier.")
   end
 
-  test 'Create review' do
+  test 'valid review' do
+    assert @review.valid?
+  end
 
+  test 'invalid without user' do
+    @review.user = nil
+    refute @review.valid?
+  end
+
+  test 'invalid without beer' do
+    @review.beer = nil
+    refute @review.valid?
+  end
+
+  test 'invalid without rating' do
+    @review.rating = nil
+    refute @review.valid?
+  end
+
+  test 'invalid with float rating' do
+    @review.rating = 5.5
+    refute @review.valid?
+  end
+
+  test 'invalid with rating of 0' do
+    @review.rating = 0
+    refute @review.valid?
+  end
+
+  test 'invalid with negative rating' do
+    @review.rating = 0
+    refute @review.valid?
+  end
+
+  test 'invalid with rating higher than 10' do
+    @review.rating = 11
+    refute @review.valid?
   end
 end
