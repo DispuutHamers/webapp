afc0f5ff7f1590bd952d92a5448a3c2c9472d1c2
Dex Bleeker <dex@bleeker.nl>
2021-09-09 17:41:08 +0200

    Do not enforce proefdatum default date because mysql sucks

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,10 @@ class Review < ActiveRecord::Base
   has_paper_trail
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10, only_integer: true }
   validates :user, uniqueness: { scope: [:user, :beer]}
+  validates :proefdatum, presence: true
   belongs_to :user
   belongs_to :beer
   acts_as_paranoid
-  attribute :proefdatum, :date, default: Date.today
 
   has_rich_text :actiontext_description
 
