52eac7e3f4959db66df7ff294a11f2c730473ac0
Dex Bleeker <dex@bleeker.nl>
2021-09-09 15:29:29 +0200

    Make reporter manditory

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,9 +2,10 @@ class Quote < ActiveRecord::Base
   has_paper_trail ignore: %i[text]
   acts_as_paranoid
   belongs_to :user
+  belongs_to :reporter, class_name: "User"
   validates :user, presence: true
+  validates :reporter, presence: true
   validates :text, presence: true
-  validate :ensure_reporter_user_exists
   serialize :text
 
   encrypts :text
@@ -12,10 +13,4 @@ class Quote < ActiveRecord::Base
   scope :ordered, -> { order('created_at DESC') }
   scope :random, -> { order('RAND()') }
   scope :with_user, -> { includes(:user) }
-
-  def ensure_reporter_user_exists
-    return unless self.reporter
-
-    errors.add('Reporter') unless User.find_by_id(self.reporter)
-  end
 end
