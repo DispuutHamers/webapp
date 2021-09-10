02dbdab3b5cafeae0c09d5ec6173b4612cb6164b
Dex Bleeker <dex@bleeker.nl>
2021-09-10 14:49:08 +0200

    Allow running of integration tests

---
+++ test/integration/root_page_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class RootPageTest < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  test 'root' do
+    visit root_path
+
+    assert page.has_content?("Hamers zonder Sikkel")
+    assert page.has_content?("defenestraties voorbehouden")
+    assert page.has_content?("Inloggen")
+    assert page.has_content?("Registreren")
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara'
+
 class ActionController::TestCase
   include Devise::Test::ControllerHelpers
 end
