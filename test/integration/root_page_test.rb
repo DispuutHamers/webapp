2f84ec8cae9c8e1d361872bee4aa2a9a0502df2f
Dex Bleeker <dex@bleeker.nl>
2021-09-10 14:53:46 +0200

    Run system tests (separate step for clarity)

--- test/integration/root_page_test.rb
+++ test/integration/root_page_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RootPageTest < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 
   test 'root' do
     visit root_path
