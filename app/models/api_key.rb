59db5a2d1adeed7e0deeb8df172d9887c5657fe6
Dex Bleeker <dex@bleeker.nl>
2021-09-10 10:40:52 +0200

    Valide presence of user and name for api key

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,6 +1,8 @@
 class ApiKey < ActiveRecord::Base
   acts_as_paranoid
   belongs_to :user
+  validates :user, presence: true
+  validates :name, presence: true, allow_blank: false
   after_initialize :set_key
 
   def set_key
--- test/models/api_key_test.rb
+++ test/models/api_key_test.rb
@@ -3,24 +3,38 @@ require 'test_helper'
 class ApiKeyTest < ActiveSupport::TestCase
   setup do
     @user = users(:one)
-    @key = ApiKey.new(user: @user)
+    @key = ApiKey.new(user: @user, name: "Key 1")
   end
 
   test 'valid api key' do
     assert @key.valid?
   end
 
+  test 'invalid without name' do
+    @key.name = nil
+    refute @key.valid?
+  end
+
+  test 'invalid with blank name' do
+    @key.name = ''
+    refute @key.valid?
+  end
+
+  test 'invalid if user does not exist' do
+    @key.user_id = -1
+    refute @key.valid?
+  end
+
   test 'creates random key' do
     @key.save
-
     assert_not_nil @key.key
   end
 
   test 'api key belongs to (single) user' do
     @key.save
 
-    key2 = ApiKey.create(user: users(:one))
-    key3 = ApiKey.create(user: users(:three))
+    key2 = ApiKey.create(user: users(:one), name: "Key 2")
+    key3 = ApiKey.create(user: users(:three), name: "Key 3")
 
     assert_equal @key.user, @user
     assert_equal key2.user, @user
@@ -29,5 +43,11 @@ class ApiKeyTest < ActiveSupport::TestCase
     assert_includes @user.api_keys, @key
     assert_includes @user.api_keys, key2
     assert_not_includes @user.api_keys, key3
+  end
+
+  test 'api key does not have versions (papertrail)' do
+    assert_raises NoMethodError do
+      @key.versions
+    end
   end
 end
