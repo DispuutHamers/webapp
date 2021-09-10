27bc04bb86e4c992a130be90b036015afc7501ba
dexbleeker <dex@bleeker.nl>
2021-09-10 20:45:36 +0200

    Rewrite action_text_migration_helper to work on meetings (#258)
    * Initial setup (very much WIP)
    
    * Rewrite action_text_migration_helper to work on meetings
    
    * On same line
    
    * Feedback

--- app/helpers/action_text_migration_helper.rb
+++ app/helpers/action_text_migration_helper.rb
@@ -1,21 +1,23 @@
 # Module to help with migration of old fields to action texts,
 # so that we can finally ditch the old columns.
-# Note to self: description is the new field, beschrijving the old one.
 module ActionTextMigrationHelper
-  def eligible_events
-    Event.all.with_rich_text_description.where.not(beschrijving: [nil, ""])
+  def eligible_meetings
+    Meeting.all.with_rich_text_actiontext_notes.where.not(notes: [nil, ""])
   end
 
-  def convert_events
-    eligible_events.each do |event|
-      if event.description.blank? && event.beschrijving
-        # Default situation -> Store beschrijving as new description
-        event.update(description: simple_format(event.beschrijving),
-                     beschrijving: nil)
-      elsif event.description && event.beschrijving
-        #  Weird situation (both fields exist) -> Delete old beschrijving
-        event.update(beschrijving: nil)
+  def convert_meetings
+    PaperTrail.request.disable_model(Meeting)
+
+    eligible_meetings.each do |meeting|
+      if meeting.actiontext_notes.blank? && meeting.notes
+        # Default situation -> Store notes as new actiontext_notes
+        meeting.update(actiontext_notes: simple_format(meeting.notes), notes: nil)
+      elsif meeting.actiontext_notes && meeting.notes
+        #  Weird situation (both fields exist) -> Delete old notes
+        meeting.update(notes: nil)
       end
     end
+
+    PaperTrail.request.enable_model(Meeting)
   end
 end
