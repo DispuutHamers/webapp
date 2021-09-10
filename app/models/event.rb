9c4db7246706807758aeef466aa070c31a856cb6
dexbleeker <dex@bleeker.nl>
2021-09-10 11:41:07 +0200

    Fix events show (#255)
    * Fix events show
    
    * Remove double description on params helper

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,7 +22,7 @@ class Event < ActiveRecord::Base
     event.dtstart = date.strftime('%Y%m%dT%H%M%S')
     event.dtend = end_time.strftime('%Y%m%dT%H%M%S')
     event.summary = title
-    event.description = description.to_plain_text || beschrijving
+    event.description = description.to_plain_text
     event.location = location
     event.ip_class = 'PUBLIC'
     event.url = "http://zondersikkel.nl/events/#{self.id}"
