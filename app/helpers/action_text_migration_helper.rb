7fbff7ed4f8af03deb7dae7dbbb158b1209018f3
Dex Bleeker <dex@bleeker.nl>
2021-09-14 14:04:25 +0200

    WIP

--- app/helpers/action_text_migration_helper.rb
+++ app/helpers/action_text_migration_helper.rb
@@ -1,23 +1,23 @@
 # Module to help with migration of old fields to action texts,
 # so that we can finally ditch the old columns.
 module ActionTextMigrationHelper
-  def eligible_meetings
-    Meeting.all.with_rich_text_actiontext_notes.where.not(notes: [nil, ""])
+  def eligible_reviews
+    Review.all.with_rich_text_actiontext_description.where.not(description: [nil, ""])
   end
 
-  def convert_meetings
-    PaperTrail.request.disable_model(Meeting)
+  def convert_reviews
+    PaperTrail.request.disable_model(Review)
 
-    eligible_meetings.each do |meeting|
-      if meeting.actiontext_notes.blank? && meeting.notes
-        # Default situation -> Store notes as new actiontext_notes
-        meeting.update(actiontext_notes: simple_format(meeting.notes), notes: nil)
-      elsif meeting.actiontext_notes && meeting.notes
-        #  Weird situation (both fields exist) -> Delete old notes
-        meeting.update(notes: nil)
+    eligible_reviews.each do |review|
+      if review.actiontext_description.blank? && review.description
+        # Default situation -> Store description as new actiontext_description
+        review.update(actiontext_description: simple_format(review.description), description: nil)
+      elsif review.actiontext_description && review.description
+        #  Weird situation (both fields exist) -> Delete old description
+        review.update(description: nil)
       end
     end
 
-    PaperTrail.request.enable_model(Meeting)
+    PaperTrail.request.enable_model(Review)
   end
 end
