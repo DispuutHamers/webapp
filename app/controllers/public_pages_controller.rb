aabdce51548f3d4be1c450c0372f27cc23992f66
Jacko Zuidema <jackozi@users.noreply.github.com>
2021-10-02 23:40:38 +0200

    Fix public page not found (#284)
    * Fix public page not found
    
    * fix test

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -14,11 +14,9 @@ class PublicPagesController < ApplicationController
   # GET /public_pages/1
   # GET /public_pages/1.json
   def show
-    raise ActionController::RoutingError.new('Not Found') if params[:id] == "robots"
+    @public_page = PublicPage.where(title: params[:id]).first
+    raise ActionController::RoutingError.new('Not Found') unless @public_page&.public || current_user&.active?
 
-    @public_page = PublicPage.where(id: params[:id]).first
-    redirect_to root_path unless @public_page
-    redirect_to root_path unless @public_page.public && !current_user&.active?
     breadcrumb @public_page.title, public_page_path(@public_page)
   end
 
