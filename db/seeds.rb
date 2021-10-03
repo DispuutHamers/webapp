98ca6784abe37a729713217dcb9f47124d532ada
Dex Bleeker <dex@bleeker.nl>
2021-10-03 17:19:29 +0200

    Cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,11 +44,9 @@ Group.create!(user_id: 5, group_id: Usergroup.find_by_name("O-lid").id)
 Group.create!(user_id: 6, group_id: Usergroup.find_by_name("A-lid").id)
 
 # Create quotes
-50.times do
-  Quote.create!(user_id: 1, text: "Turken doen aan eerwraak enzo. Negers swaffelen alleen maar", reporter_id: 2)
-  Quote.create!(user_id: 2, text: "Het mag ook een hele mooie, goed schoongemaakte penis zijn..", reporter_id: 3)
-  Quote.create!(user_id: 3, text: "Ik hou wel van enorme lullen", reporter_id: 1)
-end
+Quote.create!(user_id: 1, text: "Turken doen aan eerwraak enzo. Negers swaffelen alleen maar", reporter_id: 2)
+Quote.create!(user_id: 2, text: "Het mag ook een hele mooie, goed schoongemaakte penis zijn..", reporter_id: 3)
+Quote.create!(user_id: 3, text: "Ik hou wel van enorme lullen", reporter_id: 1)
 
 # Create beers
 Beer.create(name: "Leffe Blond", kind: "Blond", brewer: "Leffe", country: "België", percentage: "6.6 %")
