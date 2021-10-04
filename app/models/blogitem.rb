960f2c9b3d72c9df9516ef86ea50ce286712597d
Dex Bleeker <dex@bleeker.nl>
2021-10-04 15:58:35 +0200

    Cleanup actiontext_body code from blogitem

--- app/models/blogitem.rb
+++ app/models/blogitem.rb
@@ -5,13 +5,7 @@ class Blogitem < ApplicationRecord
   has_many :blogphotos, dependent: :destroy
   belongs_to :user
 
-  has_rich_text :actiontext_body
   has_rich_text :body
-
-  before_save do
-    self.body = self.actiontext_body
-    self.actiontext_body.destroy
-  end
   
   def self.default_scope
     where("length(title) > 1")
