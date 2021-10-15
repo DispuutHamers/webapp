7222c2495eafbf0897ef79e504b767f5db8c2bf7
Dex Bleeker <dex@bleeker.nl>
2021-10-15 17:26:10 +0200

    Fix meeting form

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -7,6 +7,10 @@ module FormsHelper
     "shadow-sm focus:ring-hamers-red-500 focus:border-hamers-red-500 block w-full sm:text-sm border-gray-300 rounded-md"
   end
 
+  def select_classes
+    "selectpicker mt-2 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-hamers-red-500 focus:border-hamers-red-500 sm:text-sm"
+  end
+
   def submit_classes
     "cursor-pointer inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-hamers-red-600 hover:bg-hamers-red-700 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
   end
