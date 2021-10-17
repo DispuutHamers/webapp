981ccbe6833a4da91dbb7ba314dae62ae8c91779
Dex Bleeker <dex@bleeker.nl>
2021-10-17 15:55:20 +0200

    Rename last actiontexts

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -2,4 +2,10 @@ class Brew < ApplicationRecord
   belongs_to :recipe
 
   has_rich_text :actiontext_description
+  has_rich_text :description
+
+  before_save do
+    self.description = self.actiontext_description
+    self.actiontext_description.destroy
+  end
 end
