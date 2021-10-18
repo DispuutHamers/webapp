4671cab29730bade9623bc1d973b3e7df70c0333
Dex Bleeker <dex@bleeker.nl>
2021-10-18 09:34:14 +0200

    Actiontext rename public pages

--- app/models/public_page.rb
+++ app/models/public_page.rb
@@ -1,9 +1,11 @@
 class PublicPage < ActiveRecord::Base
   has_paper_trail
   acts_as_paranoid
+  has_rich_text :content
   has_rich_text :actiontext_content
 
   before_save do
-    self.actiontext_content = self.content
+    self.content = self.actiontext_content
+    self.actiontext_content.destroy
   end
 end
