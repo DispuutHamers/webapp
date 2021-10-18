58bdbde3c66b79fd2f611c7369be0921a9991e56
Dex Bleeker <dex@bleeker.nl>
2021-10-18 09:39:27 +0200

    Only change the rich text name --> rename manually in database to prevent the need for another PR

--- app/models/public_page.rb
+++ app/models/public_page.rb
@@ -2,10 +2,4 @@ class PublicPage < ActiveRecord::Base
   has_paper_trail
   acts_as_paranoid
   has_rich_text :content
-  has_rich_text :actiontext_content
-
-  before_save do
-    self.content = self.actiontext_content
-    self.actiontext_content.destroy
-  end
 end
