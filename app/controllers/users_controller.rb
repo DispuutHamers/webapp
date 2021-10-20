6f6af3bb83b91544a8ab1fa4cec15d13632a2bbd
Dex Bleeker <dex@bleeker.nl>
2021-10-20 17:19:17 +0200

    25 items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   def show
     breadcrumb @user.name, user_path(@user)
 
-    @pagy, @quotes = pagy(@user.quotes.ordered, items: 4, page: params[:page])
+    @pagy, @quotes = pagy(@user.quotes.ordered, items: 25, page: params[:page])
     @missed_drinks = UsersHelper.missed_drinks_for(@user) if @user.lid?
   end
 
