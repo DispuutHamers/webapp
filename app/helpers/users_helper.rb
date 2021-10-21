be1dbc6442ac7ee321504b9008eba6b0d8133613
Dex Bleeker <dex@bleeker.nl>
2021-10-21 18:20:25 +0200

    Links for trail, usergroups and gravatar size increase

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,14 @@
 module UsersHelper
-  def gravatar_for(user, options = {size: 128, class: 'h-6 w-6 rounded-full'})
+  def gravatar_for(user, options = {size: 256, class: 'h-6 w-6 rounded-full'})
     gravatar_id = Digest::MD5.hexdigest(user&.email&.downcase || 'system')
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&r=x&d=monsterid"
+    pp options
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&r=x&d=monsterid"
     image_tag(gravatar_url, alt: user&.name, class: options[:class])
   end
 
-  def gravatar_for_email(email, options = {size: 128, class: 'h-6 w-6 rounded-full'})
+  def gravatar_for_email(email, options = {size: 256, class: 'h-6 w-6 rounded-full'})
     gravatar_id = Digest::MD5.hexdigest(email&.downcase || 'system')
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&r=x&d=monsterid"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&r=x&d=monsterid"
     image_tag(gravatar_url, alt: email, class: options[:class])
   end
 
