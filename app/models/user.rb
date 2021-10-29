519c673b8a4894879529efe7bd66ac9c6b100a69
Jacko Zuidema <jacko@moneybird.com>
2021-10-29 14:48:10 +0200

    move key to constant

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :invitable, :two_factor_backupable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable, :two_factor_authenticatable,
-         :otp_secret_encryption_key => Rails.application.credentials.otp_secret
+         :otp_secret_encryption_key => OTP_SECRET
   has_paper_trail ignore: %i[sunday_ratio encrypted_password reset_password_token reset_password_sent_at
 remember_created_at sign_in_count current_sign_in_at last_sign_in_at current_sign_in_ip
 last_sign_in_ip password_salt confirmation_token confirmed_at confirmation_sent_at
