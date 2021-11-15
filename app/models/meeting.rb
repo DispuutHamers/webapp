7ab1ddec3f4977f5c416be88872f3318840a678c
dexbleeker <dex@bleeker.nl>
2021-11-15 20:12:46 +0100

    Store meeting agenda as rich text (#395)

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,7 +7,12 @@ class Meeting < ActiveRecord::Base
   has_many :users, through: :attendees
   has_many :drafts, as: :entity, dependent: :destroy
 
+  has_rich_text :agenda_rt
   has_rich_text :notes
+
+  before_save do
+    self.agenda_rt = ApplicationController.helpers.simple_format(self.agenda)
+  end
 
   def save_draft(user)
     drafts.find_or_create_by(user: user).update(rich_data: notes)
