99bbbe26abd29d59c4d2a5ec9b3a142a09c7df13
Dex Bleeker <dex@bleeker.nl>
2021-11-21 13:44:03 +0100

    Remove unused columns

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,7 +1,5 @@
 class Sticker < ActiveRecord::Base
   has_paper_trail
-  has_attached_file :image
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   acts_as_paranoid
   belongs_to :user
   validates :lat, presence: true, format: { with: /\A(-?\d*\.\d*)\z/i, message: "Gast, dit is geen coördinaat"}
