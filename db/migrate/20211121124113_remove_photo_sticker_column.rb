a363409ac5d83cedab16ad05df2176d345e72571
dexbleeker <dex@bleeker.nl>
2021-11-21 14:27:43 +0100

    Improvements to stickers (#407)
    * Tailwind table and add stickers to seed
    
    * Add title, subtitle and options to personal
    
    * Cleanup html and add negative margins
    
    * Remove unused columns
    
    * Fixes to model
    
    * Use table pagination
    
    * Store address in db for quick lookup
    
    * Minor fixes

---
+++ db/migrate/20211121124113_remove_photo_sticker_column.rb
@@ -0,0 +1,9 @@
+class RemovePhotoStickerColumn < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :stickers, :picture
+    remove_column :stickers, :image_file_name
+    remove_column :stickers, :image_file_size
+    remove_column :stickers, :image_content_type
+    remove_column :stickers, :image_updated_at
+  end
+end
