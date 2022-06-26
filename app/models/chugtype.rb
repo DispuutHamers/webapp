d1a9852ec771f1a45f7cd7e3c976d3491132728b
Koen de Jong <koenjdejong@gmail.com>
2022-06-26 19:52:37 +0200

    Small updates

--- app/models/chugtype.rb
+++ app/models/chugtype.rb
@@ -2,4 +2,7 @@ class Chugtype < ActiveRecord::Base
   has_paper_trail
   has_many :chugs
   has_rich_text :description
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { maximum: 500 }
 end
