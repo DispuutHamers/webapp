a8854db0c2451dca2483e2b0e0f8469a221f8d77
Koen de Jong <koenjdejong@gmail.com>
2022-07-16 17:49:52 +0200

    Once more trying to fix unique, no success

--- app/controllers/chugtypes_controller.rb
+++ app/controllers/chugtypes_controller.rb
@@ -10,13 +10,13 @@ class ChugtypesController < ApplicationController
 
   def show
     @pagy, @chugs_all_newest = pagy(@chugtype.chugs.order('created_at DESC'), items: 10, page: params[:page])
-    # @chugs_unique = @chugtype.chugs.order('secs ASC, milis ASC, created_at ASC').distinct('user_id')
-    # @chugs_unique = @chugtype.chugs.all.group_by(&:user_id).map { |k, v| [k, v.first] }
-    @chugs_unique = Chug.all.order('secs ASC, milis ASC, created_at ASC')
-                        .select{|chug| chug.chugtype == @chugtype}
-                        .uniq{|chug| chug.user}
-                        .sort_by{|chug| [chug.secs, chug.milis]}
-
+    # @chugs_unique = @chugtype.chugs.order('secs ASC, milis ASC, created_at ASC').distinct('user_id') # distinct on user_id doesn't seem to work.
+    # @chugs_unique = @chugtype.chugs.all.group_by(&:user_id).map { |k, v| [k, v.first] } # returns array type, kinda hard to then have the link_to stuff + wanna keep it clean
+    # @chugs_unique = Chug.all.order('secs ASC, milis ASC, created_at ASC')
+    #                     .select{|chug| chug.chugtype == @chugtype}
+    #                     .uniq{|chug| chug.user}
+    #                     .sort_by{|chug| [chug.secs, chug.milis]} # somehow this is not always correct, not sure why..
+    @chugs_unique = @chugtype.chugs.order('secs ASC, milis ASC, created_at ASC')
     breadcrumb @chugtype.name, chugtype_path(@chugtype)
   end
 
