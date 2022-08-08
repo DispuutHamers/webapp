d3815ee143512ccb6a3ae002c98b7cf7a4143c1b
Koen de Jong <74268574+koenjdejong@users.noreply.github.com>
2022-08-08 16:48:00 +0200

    Merge master <- ics_event_end_date Fixes nil end_date for ics activities. Fixes #453 (#459)
    * ICS event data nullpointer fix
    
    end_time could be nil fix
    
    * Oops incorrect order within if
    
    * Also fixed end_time < begin_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,7 @@ class Event < ActiveRecord::Base
   belongs_to :usergroup
   validates :title, presence: true, allow_blank: false
   validates :date, presence: true, allow_blank: false
+  validate :begin_before_end_time
 
   scope :with_signups, -> { includes(:signups) }
   scope :upcoming, -> { where(['date >= ?', Time.zone.now]) }
@@ -21,7 +22,12 @@ class Event < ActiveRecord::Base
     url = "https://zondersikkel.nl/events/#{self.id}"
     event = Icalendar::Event.new
     event.dtstart = date.strftime('%Y%m%dT%H%M%S')
-    event.dtend = end_time.strftime('%Y%m%dT%H%M%S')
+    if end_time.nil?
+      # standard endtime is + one hour for activities, since an endtime has to be defined within an ics activity and not on our site
+      event.dtend = (date + 1.hour).strftime('%Y%m%dT%H%M%S')
+    else
+      event.dtend = end_time.strftime('%Y%m%dT%H%M%S')
+    end
     event.summary = title
     event.description = "#{description.to_plain_text} \n\n #{url}"
     event.location = location
@@ -66,5 +72,12 @@ class Event < ActiveRecord::Base
 
   def to_s
     title
+  end
+
+  private
+
+  def begin_before_end_time
+    return unless date && end_time
+    errors.add(:end_time, 'De activiteit eindigt voordat hij begint.') if date > end_time
   end
 end
