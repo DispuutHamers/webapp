eb2d57455817b6914b490e6a5ee0836c21680562
Koen de Jong <74268574+koenjdejong@users.noreply.github.com>
2022-08-16 17:43:56 +0200

    Fixes #463 (#464)
    * Can be on 1 line instead of 2
    
    * Delete all chugs when chugtype is removed
    
    * Fix #463, trail checks chugtype existance
    
    * Destroy using dependant: :destroy

--- app/models/chugtype.rb
+++ app/models/chugtype.rb
@@ -2,7 +2,7 @@ class Chugtype < ActiveRecord::Base
   acts_as_paranoid
   has_paper_trail
   has_rich_text :description
-  has_many :chugs
+  has_many :chugs, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
