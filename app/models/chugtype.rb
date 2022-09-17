f91eba7efcef947fed0c1c66b232bf689314ff1a
Koen de Jong <koenjdejong@gmail.com>
2022-09-17 18:47:34 +0200

    There is already a string longer than 32

--- app/models/chugtype.rb
+++ app/models/chugtype.rb
@@ -4,6 +4,6 @@ class Chugtype < ActiveRecord::Base
   has_rich_text :description
   has_many :chugs, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 32 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 64 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
