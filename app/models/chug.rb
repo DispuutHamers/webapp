ffabbea38e041b6c70b4faf728541d332561e47b
Koen de Jong <koenjdejong@gmail.com>
2022-10-31 17:58:32 +0100

    Added scopes to model

--- app/models/chug.rb
+++ app/models/chug.rb
@@ -8,4 +8,8 @@ class Chug < ActiveRecord::Base
   validates :user, presence: true
   validates :time, presence: true, numericality: { greater_than_or_equal_to: 0.0, less_than: 1000.0 }
   validates :comment, length: { maximum: 500 }
+
+  scope :newest, -> chugtype { where(chugtype: chugtype).order('created_at DESC') }
+  scope :unique_not_extern, -> chugtype { where(chugtype: chugtype).order('time ASC, created_at ASC').uniq { |chug| chug.user }.reject { |chug| chug.user == 7 } }
+  scope :extern, -> chugtype { where(chugtype: chugtype).select { |chug| chug.user.id == 7 } }
 end
