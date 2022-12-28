828f35562ff1ae9e43b8b8b23ce4ddc3658e31f0
Koen de Jong <koenjdejong@gmail.com>
2022-12-28 18:58:36 +0100

    Add migration and apply schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_181316) do
+ActiveRecord::Schema.define(version: 2022_12_28_175101) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", limit: 255, null: false
@@ -168,6 +168,8 @@ ActiveRecord::Schema.define(version: 2022_10_24_181316) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.text "description"
+    t.boolean "archived", default: false
     t.index ["deleted_at"], name: "index_groups_on_deleted_at"
     t.index ["group_id"], name: "index_groups_on_group_id"
     t.index ["user_id", "group_id"], name: "index_groups_on_user_id_and_group_id", unique: true
@@ -412,8 +414,8 @@ ActiveRecord::Schema.define(version: 2022_10_24_181316) do
     t.text "object", size: :long, collation: "utf8mb4_bin"
     t.text "object_changes", size: :long, collation: "utf8mb4_bin"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
-    t.check_constraint "json_valid(`object_changes`)", name: "object_changes"
-    t.check_constraint "json_valid(`object`)", name: "object"
+    t.check_constraint "son_valid(`object_changes`", name: "object_changes"
+    t.check_constraint "son_valid(`object`", name: "object"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
