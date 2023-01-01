74dbd73a1d48bdbde294205fb6e66a8215d467e5
Koen de Jong <koenjdejong@gmail.com>
2023-01-01 22:14:11 +0100

    Update migration so all versions are removed

--- db/migrate/20230101170036_add_anonymous_to_quote.rb
+++ db/migrate/20230101170036_add_anonymous_to_quote.rb
@@ -1,7 +1,15 @@
 class AddAnonymousToQuote < ActiveRecord::Migration[6.1]
   def change
-    Quote.all.each(&:save)
-    # TODO remove all records with deleted_at != nil to anonymous and remove all from version history
     add_column :quotes, :anonymous, :boolean, default: false
+
+    Quote.where(deleted_at: nil).each do |quote|
+      quote.anonymous = true
+      quote.user = nil
+      quote.reporter = nil
+      quote.save
+      quote.versions.each do |version|
+        version.destroy
+      end
+    end
   end
 end
