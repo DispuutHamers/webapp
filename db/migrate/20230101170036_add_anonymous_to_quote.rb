6e19f1dae1b57d58c2e42a74cedd2a4feb6cbb37
Koen de Jong <koenjdejong@gmail.com>
2023-01-02 11:44:35 +0100

    Big mistake in migration

--- db/migrate/20230101170036_add_anonymous_to_quote.rb
+++ db/migrate/20230101170036_add_anonymous_to_quote.rb
@@ -2,7 +2,7 @@ class AddAnonymousToQuote < ActiveRecord::Migration[6.1]
   def change
     add_column :quotes, :anonymous, :boolean, default: false
 
-    Quote.where(deleted_at: nil).each do |quote|
+    Quote.where.not(deleted_at: nil).each do |quote|
       quote.anonymous = true
       quote.user = nil
       quote.reporter = nil
