8305fea2ee35098bdc80ceee9becd7edd93995f4
Koen de Jong <koenjdejong@gmail.com>
2023-01-15 12:54:14 +0100

    Schema was wrong, now corrected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_01_142403) do
+ActiveRecord::Schema.define(version: 2023_01_01_170036) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", limit: 255, null: false
