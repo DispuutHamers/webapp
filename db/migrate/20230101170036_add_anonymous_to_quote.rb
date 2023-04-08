7f72841a3a44cdc65a7264819a0d4a1f61acb92b
Koen de Jong <koenjdejong@gmail.com>
2023-04-08 11:23:07 +0200

    Fix styling and migration

--- db/migrate/20230101170036_add_anonymous_to_quote.rb
+++ db/migrate/20230101170036_add_anonymous_to_quote.rb
@@ -1,15 +1,11 @@
 class AddAnonymousToQuote < ActiveRecord::Migration[6.1]
   def change
     add_column :quotes, :anonymous, :boolean, default: false
-
     Quote.where.not(deleted_at: nil).each do |quote|
       quote.anonymous = true
       quote.user = nil
       quote.reporter = nil
       quote.save
-      quote.versions.each do |version|
-        version.destroy
-      end
     end
   end
 end
