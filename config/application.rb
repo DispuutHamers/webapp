155d36e14b8f520b7564be9bb68c2d4c72193a2b
Jacko Zuidema <jacko@hey.com>
2025-02-05 16:14:53 +0100

    Replace secrets with credentials

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,9 @@ module Hamers
         }
       }
     }
+
+    config.active_record.encryption.primary_key = Rails.application.credentials.active_record_encryption.primary_key
+    config.active_record.encryption.deterministic_key = Rails.application.credentials.active_record_encryption.deterministic_key
+    config.active_record.encryption.key_derivation_salt = Rails.application.credentials.active_record_encryption.key_derivation_salt
   end
 end
