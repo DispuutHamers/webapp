a233b1d7a176445b807ed4fc70dd1a32b821b3fa
Stan Sjerps <stan@zondersikkel.nl>
2025-03-07 00:22:49 +0100

    dark colour scheme buttons

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -1,6 +1,6 @@
 module FrontendHelper
   def button_classes
-    "cursor-pointer inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
+    "cursor-pointer inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
   end
 
   def label_classes
@@ -24,7 +24,7 @@ module FrontendHelper
   end
 
   def user_icon_stack_image(size = 6)
-    "h-#{size} w-#{size} rounded-full bg-white ring-2 ring-white inline-block"
+    "h-#{size} w-#{size} rounded-full bg-white dark:bg-gray-800 ring-2 ring-white dark:ring-gray-800 inline-block"
   end
 
   def small_button_classes
