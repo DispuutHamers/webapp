ce98f3957788a4a3491d98ae555ddb5f6e9d85b7
Stan Sjerps <stan@zondersikkel.nl>
2025-03-07 11:25:01 +0100

    Changed gray colour on forms for dark mode

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -8,11 +8,11 @@ module FrontendHelper
   end
 
   def input_classes
-    "shadow-sm focus:ring-hamers-red-500 focus:border-hamers-red-500 block w-full sm:text-sm border-gray-300 rounded-md"
+    "shadow-sm focus:ring-hamers-red-500 focus:border-hamers-red-500 block w-full sm:text-sm border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-800"
   end
 
   def select_classes
-    "cursor-pointer selectpicker mt-2 block w-full py-2 px-3 border border-gray-300 dark:border-gray-900 bg-white dark:bg-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-hamers-red-500 focus:border-hamers-red-500 sm:text-sm dark:text-gray-200"
+    "cursor-pointer selectpicker mt-2 block w-full py-2 px-3 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-hamers-red-500 focus:border-hamers-red-500 sm:text-sm dark:text-gray-200"
   end
 
   def checkbox_classes
@@ -20,7 +20,7 @@ module FrontendHelper
   end
 
   def submit_classes
-    "cursor-pointer inline-flex items-center px-4 py-2 border border-transparent text-sm font-bold rounded-md shadow-sm text-white bg-hamers-red-500 hover:bg-hamers-red-600 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
+    "cursor-pointer inline-flex items-center px-4 py-2 border border-transparent text-sm font-bold rounded-md shadow-sm text-white bg-hamers-red-500 hover:bg-hamers-red-600 dark:hover:bg-hamers-red-400 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
   end
 
   def user_icon_stack_image(size = 6)
