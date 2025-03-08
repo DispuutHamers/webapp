87ccd8a93820f0bd412dcdf64962b86b2afac8e2
Stan Sjerps <stan@zondersikkel.nl>
2025-03-09 00:20:05 +0100

    Returned border for light mode buttons

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -1,6 +1,6 @@
 module FrontendHelper
   def button_classes
-    "cursor-pointer inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
+    "cursor-pointer inline-flex items-center px-4 py-2 border border-gray-300 dark:border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-hamers-red-500"
   end
 
   def label_classes
