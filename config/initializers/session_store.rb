d1c970f3fcad26591d8d37d72670ddb542693747
Koen de Jong <mail@koendejong.net>
2025-04-27 16:39:09 +0200

    False again...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_hamers_session', same_site: :lax, secure: :true
+  Rails.application.config.session_store :cookie_store, key: '_hamers_session', same_site: :lax, secure: :false
 else
   Rails.application.config.session_store :cookie_store
 end
