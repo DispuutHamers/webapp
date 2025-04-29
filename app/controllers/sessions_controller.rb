75906d033636dd5d61b4cac90907a64ad2dd2ad5
Koen de Jong <mail@koendejong.net>
2025-04-29 11:17:03 +0200

    Remove comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate(auth_options)
-    puts "#{auth_options} #{warden.inspect} #{warden.authenticated?(resource_name)} #{warden.user(resource_name)}"
     if resource
       set_flash_message!(:notice, :signed_in)
       sign_in(resource_name, resource)
