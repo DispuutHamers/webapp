0becf91d95c912129bf2de9f04f3e7d3990f0cc1
Koen de Jong <mail@koendejong.net>
2025-04-30 12:57:40 +0200

    Enable cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 if [ -f tmp/pids/server.pid ]; then
+  echo "Removing pre-existing server.pid..."
   rm tmp/pids/server.pid
 fi
 
@@ -10,7 +11,10 @@ echo "Apply database migrations..."
 bundle exec rails db:migrate
 
 echo "Writing schedule..."
-bundle exec whenever -w
+bundle exec whenever --update-crontab
+
+echo "Starting cron..."
+cron -f &
 
 echo "Starting server..."
 bundle exec puma -C config/puma.rb
