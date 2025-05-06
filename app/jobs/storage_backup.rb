51016a151b14815a1ba51bb114c3d1a28bdb4c49
Koen de Jong <mail@koendejong.net>
2025-05-06 14:33:03 +0200

    Add storage backup

--- app/jobs/storage_backup.rb
+++ app/jobs/storage_backup.rb
@@ -1,7 +1,7 @@
 require 'google/apis/drive_v3'
 require 'googleauth'
 
-class DatabaseBackup < ApplicationJob
+class StorageBackup < ApplicationJob
   queue_as :default
 
   def perform
@@ -11,23 +11,27 @@ class DatabaseBackup < ApplicationJob
       scope: 'https://www.googleapis.com/auth/drive'
     )
 
-    filename = "database_backup_#{Time.now.strftime('%Y-%m-%d_%H:%M:%S')}.sqlite3"
-    database_file = Google::Apis::DriveV3::File.new(name: filename, parents: ['1fP9MXN2lhypkzaxIYyy7ZHb7e_M1jD5a'])
-    result = drive.create_file(database_file, upload_source: 'db/production.sqlite3', supports_team_drives: true)
+    filename = "storage_backup_#{Time.now.strftime('%Y-%m-%d_%H:%M:%S')}.tar.gz"
+    storage_file = Google::Apis::DriveV3::File.new(name: filename, parents: ['1K8x_2ZMNa7J0nZtCLZ_PRY_GCODWmNx-'])
+    temp_file = "#{Rails.root}/tmp/#{filename}"
+    system("tar -czf #{temp_file} -C #{Rails.root}/storage .")
+    result = drive.create_file(storage_file, upload_source: temp_file, supports_team_drives: true)
+    File.delete(temp_file) if File.exist?(temp_file)
+
     if result.id
       puts "Database backup uploaded successfully"
 
-      # delete files older than 60 days in Shared Drive 'Developers' -> 'database_backups'
+      # delete files older than 60 days in Shared Drive 'Developers' -> 'storage backups'
       drive.list_files(
         drive_id: '0ADfPfTkhdOdoUk9PVA',
-        q: "name contains 'database_backup_'",
+        q: "name contains 'storage_backup_'",
         corpora: 'drive',
         include_items_from_all_drives: true,
         supports_team_drives: true,
-      ).files.each do |file|
+        ).files.each do |file|
         puts "Checking file #{file.name}"
-        if file.name.match?(/database_backup_(\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2})\.sqlite3/)
-          date = Date.strptime(file.name, 'database_backup_%Y-%m-%d_%H:%M:%S.sqlite3')
+        if file.name.match?(/storage_backup_(\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2})\.sqlite3/)
+          date = Date.strptime(file.name, 'storage_backup_%Y-%m-%d_%H:%M:%S.sqlite3')
           if date < (Time.now - 60.days)
             puts "Deleting old backup #{file.name}"
             drive.delete_file(file.id, supports_team_drives: true)
@@ -38,7 +42,7 @@ class DatabaseBackup < ApplicationJob
       end
 
       # Call check if backup was successful
-      Net::HTTP.get(URI.parse('https://api.honeybadger.io/v1/check_in/xoIqO4'))
+      Net::HTTP.get(URI.parse('https://api.honeybadger.io/v1/check_in/b3Ije4'))
       true
     else
       puts "Error uploading database backup"
